cd02eee15835bb1e2e83e5476f59c8501be1eef6
Mohit Gawande <mohitgawande749@gmail.com>
2025-12-29 18:22:05 +0530

    Update Contact -> page.jsx

--- Contact -> page.jsx
+++ Contact -> page.jsx
@@ -2,7 +2,7 @@
 
 export default function Contact() {
   return (
-    <div className="p-4 space-y-4">
+    <div className="p-4 space-y-4 page-enter">
       <div className="text-lg font-semibold text-theme">Contact Us</div>
       <hr className="border-theme/30 mb-4" />
 
